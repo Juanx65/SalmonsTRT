aafc6c5e5221dad1b7c9d813563dde4764d97820
juanx65 <juan.aguileraca@sansano.usm.cl>
2024-12-28 22:23:14 -0300

    build defaut

--- build_default.py
+++ build_default.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 model = YOLO('weights/yolov8lsalmons.pt')  # load a custom trained model
 
 # Export the model
-model.export(format='engine', int8=True, half=False,dynamic=True,batch=32)
+model.export(format='engine', int8=False, half=True,dynamic=True,batch=256,imgsz=320, data='datasets/salmons/salmons.yaml')
