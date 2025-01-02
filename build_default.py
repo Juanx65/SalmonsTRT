2c1c3da652b59f2190db83051e4516e9b92e6265
juanx65 <juan.aguileraca@sansano.usm.cl>
2025-01-02 17:04:09 -0300

    mAP caracterization para medir una espeice de closeness segun las mascaras de segmentacion

--- build_default.py
+++ build_default.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO('weights/yolov8lsalmons.pt')  # load a custom trained model
+model = YOLO('weights/yolov11salmons.pt')  # load a custom trained model
 
 # Export the model
-model.export(format='engine', int8=False, half=True,dynamic=True,batch=256,imgsz=320, data='datasets/salmons/salmons.yaml')
+model.export(format='engine', int8=False, half=False,dynamic=True,batch=128,imgsz=640, data='datasets/salmons/salmons.yaml')
